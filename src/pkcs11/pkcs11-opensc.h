67a232059e2e80b8223e71ca53e42df625c94011
Thomas Calderon <thomas.calderon@ssi.gouv.fr>
2015-01-16 17:06:02 +0100

    PKCS#11: Update OpenSC to use PKCS#11 v2.20A3
      * Use PKCS#11 v2.20 amendment 3 until v.2.40 is out.

--- src/pkcs11/pkcs11-opensc.h
+++ src/pkcs11/pkcs11-opensc.h
@@ -9,4 +9,16 @@
  */
 #define CKA_OPENSC_NON_REPUDIATION      (CKA_VENDOR_DEFINED | 1UL)
 
+/* GOSTR defines, not in PKCS#11 v2.20, move them here for now */
+#define CKA_GOSTR3410_PARAMS            (0x250UL)
+#define CKA_GOSTR3411_PARAMS            (0x251UL)
+#define CKA_GOST28147_PARAMS            (0x252UL)
+
+#define CKK_GOSTR3410           (0x30UL)
+
+#define CKM_GOSTR3410_KEY_PAIR_GEN      (0x1200UL)
+#define CKM_GOSTR3410                   (0x1201UL)
+#define CKM_GOSTR3410_WITH_GOSTR3411    (0x1202UL)
+#define CKM_GOSTR3411                   (0x1210UL)
+
 #endif
